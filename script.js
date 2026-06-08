3fd1f2ac9b384262af2fee00e1c173badc4a0083
SG-Youth-Club <lcohen@spectrumgaming.net>
2026-06-08 11:24:36 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -80,7 +80,7 @@ function exportPDF() {
             quality: 1
         },
         html2canvas: {
-            scale: 2,
+            scale: 1,
             backgroundColor: "#12002e"
         },
         jsPDF: {
